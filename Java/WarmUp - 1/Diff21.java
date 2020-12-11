e2ef299414f9b42ca0e0b76d91c33a5c4b95d307
Utkarsh1311 <itsutkarsh123@gmail.com>
2020-12-11 23:29:22 +0530

    made some changes to Diff21

--- Java/WarmUp - 1/Diff21.java
+++ Java/WarmUp - 1/Diff21.java
@@ -8,7 +8,9 @@
 
 */
 
-public int diff21(int n) 
-{
-  return n <= 21 ? Math.abs(n -21) : 2*(Math.abs(n - 21));
+public class Diff21 {
+    public int diff21(int n) 
+    {
+        return n <= 21 ? Math.abs(n -21) : 2*(Math.abs(n - 21));
+    }
 }
