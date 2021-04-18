1499a1903c753b117afefe83557759d63956cb33
Utkarsh <itsutkarsh123@gmail.com>
2021-04-18 11:44:50 +0530

    added endUp solution

---
+++ Java/WarmUp - 1/EndUp.java
@@ -0,0 +1,9 @@
+
+public class EndUp {
+    public String endUp(String str) {
+
+        return str.length() <= 3 ? str.toUpperCase()
+                : str.substring(0, str.length() - 3) + str.substring(str.length() - 3).toUpperCase();
+    }
+
+}
