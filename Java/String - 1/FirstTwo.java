ea2a6eda1283a1b26c633c6fd75d1ce96f96ee0e
Utkarsh <itsutkarsh123@gmail.com>
2021-04-19 20:19:46 +0530

    added firstTwo solution

---
+++ Java/String - 1/FirstTwo.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Given a string, return the string made of its first two chars, so the String
+ * "Hello" yields "He". If the string is shorter than length 2, return whatever
+ * there is, so "X" yields "X", and the empty string "" yields the empty string
+ * "". Note that str.length() returns the length of a string.
+ * 
+ * 
+ * firstTwo("Hello") → "He" firstTwo("abcdefg") → "ab" firstTwo("ab") → "ab"
+ */
+
+public class FirstTwo {
+    public String firstTwo(String str) {
+        return str.length() <= 2 ? str : str.substring(0, 2);
+    }
+
+}
