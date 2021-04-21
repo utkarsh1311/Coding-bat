7c2e88be1daab9b69310813d7956eddc25706acd
Utkarsh <itsutkarsh123@gmail.com>
2021-04-21 20:48:30 +0530

    added comboString solution

---
+++ Java/String - 1/ComboString.java
@@ -0,0 +1,16 @@
+/**
+ * Given 2 strings, a and b, return a string of the form short+long+short, with
+ * the shorter string on the outside and the longer string on the inside. The
+ * strings will not be the same length, but they may be empty (length 0).
+ * 
+ * 
+ * comboString("Hello", "hi") → "hiHellohi" comboString("hi", "Hello") →
+ * "hiHellohi" comboString("aaa", "b") → "baaab"
+ */
+
+public class ComboString {
+    public String comboString(String a, String b) {
+        return a.length() > b.length() ? (b + a + b) : (a + b + a);
+    }
+
+}
