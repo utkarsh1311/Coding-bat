8c1f39e625241ed6804b97b02fcfe7f7b265fb60
Utkarsh <itsutkarsh123@gmail.com>
2021-04-29 21:46:02 +0530

    added hasBad solution

---
+++ Java/String - 1/HasBad.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * Given a string, return true if "bad" appears starting at index 0 or 1 in the
+ * string, such as with "badxxx" or "xbadxx" but not "xxbadxx". The string may
+ * be any length, including 0. Note: use .equals() to compare 2 strings.
+ * 
+ * 
+ * hasBad("badxx") → true hasBad("xbadxx") → true hasBad("xxbadxx") → false
+ */
+
+public class HasBad {
+    public boolean hasBad(String str) {
+        if (str.length() >= 3 && str.substring(0, 3).equals("bad")) {
+            return true;
+        }
+        if (str.length() >= 4 && str.substring(1, 4).equals("bad")) {
+            return true;
+        }
+        return false;
+    }
+}
