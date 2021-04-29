bf36d42b6aec496bb21de030f9904914c0f5c4f9
Utkarsh <itsutkarsh123@gmail.com>
2021-04-29 21:47:23 +0530

    added atFirst solution

---
+++ Java/String - 1/AtFirst.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Given a string, return a string length 2 made of its first 2 chars. If the
+ * string length is less than 2, use '@' for the missing chars.
+ * 
+ * 
+ * atFirst("hello") → "he" atFirst("hi") → "hi" atFirst("h") → "h@"
+ * 
+ */
+
+public class AtFirst {
+    public String atFirst(String str) {
+        if (str.length() == 0) {
+            return "@@";
+        } else if (str.length() == 1) {
+            return str + "@";
+        }
+        return str.substring(0, 2);
+    }
+}
