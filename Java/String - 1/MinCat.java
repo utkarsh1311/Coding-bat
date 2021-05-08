a0be6053edce5e3cbc7309b405df754431bbeb7c
Utkarsh <itsutkarsh123@gmail.com>
2021-05-08 23:22:48 +0530

    added minCat solution

---
+++ Java/String - 1/MinCat.java
@@ -0,0 +1,21 @@
+/**
+ * Given two strings, append them together (known as "concatenation") and return
+ * the result. However, if the strings are different lengths, omit chars from
+ * the longer string so it is the same length as the shorter string. So "Hello"
+ * and "Hi" yield "loHi". The strings may be any length.
+ * 
+ * 
+ * minCat("Hello", "Hi") → "loHi" minCat("Hello", "java") → "ellojava"
+ * minCat("java", "Hello") → "javaello"
+ */
+
+public class MinCat {
+    public String minCat(String a, String b) {
+        if (a.length() < b.length()) {
+            return a + b.substring(b.length() - a.length());
+        } else if (a.length() > b.length()) {
+            return a.substring(a.length() - b.length()) + b;
+        }
+        return a + b;
+    }
+}
