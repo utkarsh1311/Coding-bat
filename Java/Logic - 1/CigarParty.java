f2df1d54356e1f9da3794ffd0bb8204803398cb4
Utkarsh <itsutkarsh123@gmail.com>
2021-05-13 22:30:36 +0530

    added cigarParty solution

---
+++ Java/Logic - 1/CigarParty.java
@@ -0,0 +1,24 @@
+/**
+ * When squirrels get together for a party, they like to have cigars. A squirrel
+ * party is successful when the number of cigars is between 40 and 60,
+ * inclusive. Unless it is the weekend, in which case there is no upper bound on
+ * the number of cigars. Return true if the party with the given values is
+ * successful, or false otherwise.
+ * 
+ * 
+ * cigarParty(30, false) → false cigarParty(50, false) → true cigarParty(70,
+ * true) → true
+ * 
+ */
+
+public class CigarParty {
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if (!isWeekend && cigars >= 40 && cigars <= 60) {
+            return true;
+        } else if (isWeekend && cigars >= 40) {
+            return true;
+        }
+        return false;
+    }
+
+}
